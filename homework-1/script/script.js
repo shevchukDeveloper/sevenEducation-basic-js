155782d0553fb58cccb9f772f7bb5d136fa9b44f
Горбаченко Олег <62697935+kykyshka69@users.noreply.github.com>
2022-04-16 23:04:31 +0300

    hw-1

---
+++ homework-1/script/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+document.write(`
+<div class="wrapper">
+      <header class="header">header 800*200</header>
+      <aside class="aside">nav 100*400</aside>
+      <main class="main">section 700*400
+       <div>
+ <ul>
+ Данні про користувача :
+<li>Ім'я: ${userName}</li>
+<li>Прізвище: ${userSecondName}</li>
+<li>Вік: ${userAge}</li>
+ </ul>
+ </div>
+      </main>
+      <footer class="footer">footer 800 *200</footer>
+    </div>
+`);
+
+ let firstNumber = +prompt("Введіть перше число");
+ let secondNumber = +prompt("Введіть друге число");
+ let lastNumber = +prompt("Введіть останне число");
+//  отримуєм доступ до дом елементу
+ let footer = document.querySelector('.footer');
+// обчислюємо дані
+let result = (firstNumber + secondNumber + lastNumber) / 3;
+//  виводим дані 
+footer.innerHTML =
+    `
+     <div>
+    Середне арифметичне число : ${result}
+     </div>
+     `;
